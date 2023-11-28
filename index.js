5f4f26d6a0b22586d9f35a540d80be0523a15489
ERIC BESSON <e-besson@laposte.net>
2023-11-28 16:39:54 +0100

    secondmodif

---
+++ index.js
@@ -0,0 +1,11 @@
+icons.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+const links = document.querySelectorAll("navbar li");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+});
